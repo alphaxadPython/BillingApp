90df26f30c6dfc2f71ef07d5ebec259f3813ad2c
alphajozee <alphajozee8@gmail.com>
2021-07-03 15:23:06 +0300

    bill

---
+++ BillingApplication/src/billingapplication/DashboardController.java
@@ -0,0 +1,15 @@
+
+package billingapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class DashboardController implements Initializable {
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
